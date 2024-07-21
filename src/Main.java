75ba6b4f3e16f4913d20789c5482da8c02b09be3
Elksandro2 <silvaelksandro@gmail.com>
2024-07-20 21:36:09 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Pilha<Integer> pilha = new Pilha<>();
+
+        for (int i=0; i<10; i++){
+            int num = sc.nextInt();
+            if (num % 2 == 0){
+                pilha.empilhar(num);
+                System.out.println("Empilhando: " + num);
+            }
+            else{
+                if (pilha.estaVazio()) System.out.println("A pilha está vazia");
+                else System.out.println("Desempilhando: " + pilha.desempilhar());
+            }
+
+        }
+        System.out.println("\nComo ficou a pilha: " + pilha);
+
+        System.out.println("\nDesempilhando o que sobrou...");
+        while(!pilha.estaVazio()){
+            System.out.println("Desempilhando: " + pilha.desempilhar());
+        }
+        System.out.println("\nA pilha está vazia: " + pilha);
+
+    }
+}
---
+++ src/Pilha.java
@@ -0,0 +1,57 @@
+public class Pilha<T> {
+
+    private T[] elementos;
+    private int tamanho;
+
+    public Pilha() {
+        this(10);
+    }
+    public Pilha(int capacidade) {
+        this.elementos = (T[]) new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public int size() {
+        return tamanho;
+    }
+
+    public boolean estaVazio(){
+        return this.tamanho == 0;
+    }
+
+    public boolean empilhar(T elemento){
+        if (this.tamanho < this.elementos.length){
+            this.elementos[this.tamanho++] = elemento;
+            return true;
+        }
+        return false;
+    }
+
+    public T desempilhar(){
+        if (!this.estaVazio()){
+            return this.elementos[--tamanho];
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i=0; i<this.tamanho-1; i++){
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+        if (this.tamanho>0){
+            s.append(this.elementos[this.tamanho-1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
+    }
+
+}
